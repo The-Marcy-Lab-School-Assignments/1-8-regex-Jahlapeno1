d4261eecec3f3ce0616c2eeef9d3bc1adebbab89
Jahlapeno1 <jimenezaot@gmail.com>
2024-04-18 18:30:25 -0400

    Finished q1-10

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,34 +1,41 @@
-const helloWorldRegex = (str) => {};
+const helloWorldRegex = (str) => {
+  return /hello world/gi.test(str)
+};
 
-const hasAVowel = (str) => {};
+const hasAVowel = (str) => {
+  return /[aeiou]/gi.test(str)
+};
 
-const hasCatsOrDogs = (str) => {};
+const hasCatsOrDogs = (str) => /(cats|dogs)/i.test(str);
 
-const hasVowelStart = (str) => {};
+const hasVowelStart = (str) => /^[aeiou]/i.test(str);
 
-const hasPunctuationEnd = (str) => {};
+const hasPunctuationEnd = (str) => /[.!?]$/.test(str);
 
-const hasNothingOrDigits = (str) => {};
+const hasNothingOrDigits = (str) => /^(\d+|)$/.test(str);
 
-const hasNoFlippers = (str) => {};
+const hasNoFlippers = (str) => /^[^BCcDEHIKOoXxl]*$/.test(str);
 
-const isValidEmail = (str) => {};
+const isValidEmail = (str) => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(str);
 
-const isValidPhoneNumber = (str) => {};
+const isValidPhoneNumber = (str) => /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]+[0-9]{3}[-\s\.]+[0-9]{4}$/.test(str);
 
-const matchAllNumbers = (str) => {};
+const matchAllNumbers = (str) => {
+  const emptyArr = []
+  return str.match(/[0-9]+/g) === null ? emptyArr : str.match(/[0-9]+/g)
+};
 
-const matchAllNumbersAsNumbers = (str) => {};
+const matchAllNumbersAsNumbers = (str) => { };
 
-const matchAllWords = (str) => {};
+const matchAllWords = (str) => { };
 
-const replaceAllNumbers = (str) => {};
+const replaceAllNumbers = (str) => { };
 
-const fixFileName = (str) => {};
+const fixFileName = (str) => { };
 
-const nameRedacter = (str) => {};
+const nameRedacter = (str) => { };
 
-const camelToSnakeCase = (str) => {};
+const camelToSnakeCase = (str) => { };
 
 module.exports = {
   helloWorldRegex,
